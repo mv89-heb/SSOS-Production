6c990e11e6043e7087d0b11c57c8a1f96634386f
mv89-heb <valanimattan89@gmail.com>
2026-07-18 23:37:31 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,31 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin();
+
+const withNextIntl =
+  createNextIntlPlugin(
+    "./src/i18n/request.ts"
+  );
+
 
 const nextConfig: NextConfig = {
+
   reactStrictMode: true,
-  
-  // הגדרת Rewrite Proxy המאפשרת פיתוח וריצה חלקים מול שרת ה-Render ללא בעיות CORS
+
+
   async rewrites() {
+
     return [
       {
         source: "/api/:path*",
-        destination: "https://ssos-backend.onrender.com/api/:path*",
+        destination:
+          "https://ssos-backend.onrender.com/api/:path*",
       },
     ];
+
   },
+
 };
+
 
 export default withNextIntl(nextConfig);
