02c7fd300c0940a87c4c9261e54ef3ca204fe8c2
mv89-heb <valanimattan89@gmail.com>
2026-07-18 23:09:35 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+// next.config.ts
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
@@ -5,6 +6,8 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  
+  // הזרקת חוק ה-Rewrites עבור ה-Proxy
   async rewrites() {
     return [
       {
